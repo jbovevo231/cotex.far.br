63ad926b787702e1b6ccd2ea2d6e0097210c3be6
Janderson Brito <jandersonpharma@gmail.com>
2026-07-22 15:30:27 -0300

    Atualização

--- database/connection.py
+++ database/connection.py
@@ -4,14 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
 def get_db():
 
     url = os.getenv("TURSO_DATABASE_URL")
     token = os.getenv("TURSO_AUTH_TOKEN")
 
-    return libsql.connect(
+    print("URL:", url)
+    print("TOKEN:", "OK" if token else "NÃO ENCONTRADO")
+
+    db = libsql.connect(
         "local.db",
         sync_url=url,
         auth_token=token
     )
+
+    db.sync()
+
+    return db
