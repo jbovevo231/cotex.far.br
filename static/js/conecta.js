3af2947662abcbcca9a9b96ff35f7832065946d4
Janderson Brito <jandersonpharma@gmail.com>
2026-08-09 10:53:41 -0300

    Atualização

--- static/js/conecta.js
+++ static/js/conecta.js
@@ -1,36 +1,5 @@
-console.log("conecta.js carregado");
+document.addEventListener("DOMContentLoaded", function () {
 
-const foto = document.getElementById("foto");
-const preview = document.getElementById("previewImagem");
-const imagem = document.getElementById("imagemPreview");
+    console.log("Conecta carregado com sucesso.");
 
-if (foto) {
-
-    foto.addEventListener("change", function () {
-
-        console.log("Imagem selecionada");
-
-        if (this.files.length === 0) return;
-
-        const reader = new FileReader();
-
-        reader.onload = function (e) {
-
-            imagem.src = e.target.result;
-            preview.style.display = "block";
-
-        };
-
-        reader.readAsDataURL(this.files[0]);
-
-    });
-
-}
-
-function removerImagem() {
-
-    foto.value = "";
-    imagem.src = "";
-    preview.style.display = "none";
-
-}
+});
